993f6d17cf05e7339855b93f42c910540cfbb7b6
Wojtysek <vojtahr1@post.cz>
2022-02-28 14:06:37 +0100

    Přidejte soubory projektu.

---
+++ funkce 3/funkce 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int global = 66; //promenna plati ve vsech funkcich
+
+void prazdna(void) {
+	//nic
+}
+
+void vytiskni(int cislo) {
+	//promena plati pouze v teto funkci
+	printf(" %d", cislo);
+	prazdna();
+	//semka...2
+	prazdna();
+	printf("%d", cislo);
+}
+
+int main() {
+
+	int cislo = 3;
+	vytiskni(cislo);
+	//tudle...1
+	vytiskni(cislo);
+}
